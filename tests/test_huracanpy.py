9ea431df2fee5e6b1618e96f56dc8990ba06505f
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-04-26 10:43:36 +0100

    Add test for csv files

--- tests/test_huracanpy.py
+++ tests/test_huracanpy.py
@@ -4,3 +4,9 @@ import huracanpy
 def test_load_track():
     data = huracanpy.load(huracanpy.example_TRACK_file, tracker="TRACK")
     assert len(data.groupby("track_id")) == 2
+
+def test_load_csv():
+    data = huracanpy.load(huracanpy.example_csv_file, tracker = "tempestextremes")
+    assert len(data) == 13
+    assert len(data.obs) == 99
+    assert len(data.groupby("track_id")) == 3
