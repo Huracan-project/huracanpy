61c9cc9559844c445c4755d0530fdcdd15e1a4dd
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-08 15:37:15 +0100

    Implement tests for lifecycle functions

--- tests/test_huracanpy.py
+++ tests/test_huracanpy.py
@@ -135,3 +135,15 @@ def test_translation_speed():
     v = huracanpy.diags.translation_speed.translation_speed(data)
     assert 6 <= v.translation_speed.mean() <= 6.1
     assert len(v.mid_obs) == len(data.obs) - data.track_id.to_dataframe().nunique()
+    
+def test_time_from_genesis():
+    data = huracanpy.load(huracanpy.example_csv_file, tracker = "csv")
+    t = huracanpy.diags.lifecycle.time_from_genesis(data)
+    assert t.min() == 0
+    assert t.max() == 1166400000000000
+    
+def test_time_from_extremum():
+    data = huracanpy.load(huracanpy.example_csv_file, tracker = "csv")
+    t = huracanpy.diags.lifecycle.time_from_extremum(data, "wind10", "max")
+    assert t.min() == -972000000000000
+    assert t.max() == 367200000000000
