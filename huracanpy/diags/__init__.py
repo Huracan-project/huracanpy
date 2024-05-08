1938b10238ab0a88bae18427c541bebcecf6fd63
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-08 22:13:35 +0100

    Add __all__

--- huracanpy/diags/__init__.py
+++ huracanpy/diags/__init__.py
@@ -1,5 +1,7 @@
 """ Huracanpy module for tracks diagnostics """
 
+__all__ = ["track_density", "track_stats", "translation_speed", "lifecycle"]
+
 from . import track_density
 from . import track_stats
 from . import translation_speed
