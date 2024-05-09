676369d41d167733151a06f94f68e920625fba81
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-09 13:52:29 +0100

    Fix problem with csv test

--- tests/test_huracanpy.py
+++ tests/test_huracanpy.py
@@ -28,6 +28,7 @@ def test_load_netcdf():
     "filename,tracker",
     [
         (huracanpy.example_TRACK_file, "TRACK"),
+        (huracanpy.example_TRACK_netcdf_file, None),
         (huracanpy.example_csv_file, None),
     ],
 )
@@ -54,7 +55,6 @@ def test_save_netcdf(filename, tracker, tmp_path):
 
 @pytest.mark.parametrize("filename,tracker", [
     (huracanpy.example_TRACK_file, "TRACK"),
-    (huracanpy.example_TRACK_netcdf_file, None),
     (huracanpy.example_csv_file, None),
 ])
 def test_save_csv(filename, tracker, tmp_path):
