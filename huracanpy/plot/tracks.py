5a8f196b809604e80f0ba3f52fde40c5cedb1ab8
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-21 15:14:50 +0100

    Test and improve plot_tracks_basic

--- huracanpy/plot/tracks.py
+++ huracanpy/plot/tracks.py
@@ -8,15 +8,19 @@ import cartopy.crs as ccrs
 
 
 def plot_tracks_basic(
-    tracks, intensity_var=None, projection=ccrs.PlateCarree(180), cmap="nipy_spectral"
+    tracks,
+    intensity_var=None,
+    subplot_kws=dict(projection=ccrs.PlateCarree(180)),
+    fig_kws=dict(figsize=(10, 10)),
+    scatter_kws=dict(palette="nipy_spectral", s=2),
 ):
     assert "lon" in list(tracks.keys()), "lon is not present in the data"
     assert "lat" in list(tracks.keys()), "lat is not present in the data"
 
-    fig, ax = plt.subplots(subplot_kw=dict(projection=projection))
+    fig, ax = plt.subplots(subplot_kw=subplot_kws, **fig_kws)
     ax.coastlines()
     sns.scatterplot(
-        data=tracks, x="lon", y="lat", hue=intensity_var, ax=ax, s=2, palette=cmap
+        data=tracks, x="lon", y="lat", hue=intensity_var, ax=ax, **scatter_kws
     )
 
     return fig, ax
