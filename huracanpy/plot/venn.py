79215bf49f89c56cf72bb19994775ccb56724cc8
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-24 16:22:55 +0100

    Implement function to plot venn diagram for 2 datasets

---
+++ huracanpy/plot/venn.py
@@ -0,0 +1,14 @@
+"""Venn diagrams for tracks matching visualisation"""
+
+from matplotlib_venn import venn2, venn2_circles
+import numpy as np
+
+
+def plot_venn_2datasets(
+    data1, data2, match, colors=("w", "w"), labels=None, circle_color="k"
+):
+    N1 = len(np.unique(data1.track_id.values))  # Number of tracks in dataset 1
+    N2 = len(np.unique(data2.track_id.values))  # Number of tracks in dataset 2
+    m = len(match)  # Number of tracks matching
+    venn2((N1 - m, N2 - m, m), set_colors=colors, set_labels=labels)
+    venn2_circles((N1 - m, N2 - m, m), color=circle_color)
