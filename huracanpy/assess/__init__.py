84a7b29b020facafe94bf07371d04ab027bbe633
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-24 16:12:21 +0100

    Implement POD and FAR functions

--- huracanpy/assess/__init__.py
+++ huracanpy/assess/__init__.py
@@ -1,5 +1,6 @@
 """Huracanpy module for tracker assessment"""
 
-__all__ = ["match_tracks"]
+__all__ = ["match_tracks", "POD", "FAR"]
 
 from .match import match_tracks
+from .scores import POD, FAR
