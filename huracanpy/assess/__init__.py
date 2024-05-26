55a9e61bd7f88dcfbe30461062c7eb60659b109a
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-26 21:44:22 +0100

    Implement overlap function for tracker assessment

--- huracanpy/assess/__init__.py
+++ huracanpy/assess/__init__.py
@@ -1,6 +1,7 @@
 """Huracanpy module for tracker assessment"""
 
-__all__ = ["match_pair", "match_multiple", "POD", "FAR"]
+__all__ = ["match_pair", "match_multiple", "POD", "FAR", "overlap"]
 
 from .match import match_pair, match_multiple
 from .scores import POD, FAR
+from .overlap import overlap
