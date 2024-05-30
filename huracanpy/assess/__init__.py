3f4f0faaa153e1c81ef6bed1b5b0da5a187459d9
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-05-30 18:28:06 +0200

    Add overlap to init

--- huracanpy/assess/__init__.py
+++ huracanpy/assess/__init__.py
@@ -1,6 +1,7 @@
 """Huracanpy module for tracker assessment"""
 
-__all__ = ["match_pair", "match_multiple", "scores"]
+__all__ = ["match_pair", "match_multiple", "scores", "overlap"]
 
 from ._match import match_pair, match_multiple
 from . import scores
+from .overlap import overlap
