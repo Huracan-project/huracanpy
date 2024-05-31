45568e82e0de2be28dacd790e0127a8b43dfe8ee
Leo Saffin <string_buster@hotmail.com>
2024-05-31 16:36:57 +0100

    Add functions for land/sea and country along track. - Based on a generic function for natural Earth data via cartopy, so more could be added in future. - Only works with data with longitude in range -180 to 180, so need to add some more tests

--- huracanpy/utils/geography.py
+++ huracanpy/utils/geography.py
@@ -2,10 +2,15 @@
 Utils related to geographical attributes
 """
 
+import warnings
+from pint.errors import UnitStrippedWarning
+
 import numpy as np
 import pandas as pd
+import shapely
 from shapely.geometry import Point
 import geopandas as gpd
+from cartopy.io.shapereader import natural_earth
 from metpy.xarray import preprocess_and_wrap
 
 from ._basins import basins_def
@@ -72,3 +77,57 @@ def get_basin(lon, lat, convention="WMO"):
         .index_right
     )  # Select basin names
     return basin
+
+
+@preprocess_and_wrap(wrap_like="lon")
+def _get_natural_earth_feature(lon, lat, feature, category, name, resolution):
+    fname = natural_earth(resolution=resolution, category=category, name=name)
+    df = gpd.read_file(fname)
+
+    # The metpy wrapper converting to pint causes errors, but I'm still going to use it
+    # because it lets me pass different array_like types for lon/lat without writing
+    # our own wrapper. For now, just convert anything not a numpy array to a numpy array
+    if not isinstance(lon, np.ndarray):
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=UnitStrippedWarning)
+            lon = np.array(lon)
+            lat = np.array(lat)
+
+    # Any strings are loaded in as objects. Use the specific string type with the
+    # maximum possible length for the output instead
+    dtype = df[feature].dtype
+    if dtype == "O":
+        max_length = df[feature].apply(len).max()
+        dtype = f"U{max_length}"
+
+    result = np.zeros(len(lon), dtype=dtype)
+    for n, row in df.iterrows():
+        result[np.where(shapely.contains_xy(row.geometry, lon, lat))] = row[feature]
+
+    return result
+
+
+def get_land_or_ocean(lon, lat, resolution="10m"):
+    is_ocean = _get_natural_earth_feature(
+        lon,
+        lat,
+        feature="featurecla",
+        category="physical",
+        name="ocean",
+        resolution=resolution,
+    )
+
+    is_ocean[is_ocean == ""] = "Land"
+
+    return is_ocean
+
+
+def get_country(lon, lat, resolution="10m"):
+    return _get_natural_earth_feature(
+        lon,
+        lat,
+        feature="NAME",
+        category="cultural",
+        name="admin_0_countries",
+        resolution=resolution,
+    )
--- tests/test_utils/test_geography.py
+++ tests/test_utils/test_geography.py
@@ -14,3 +14,21 @@ def test_basin():
 
     assert (result[:51] == "AUS").all()
     assert (result[51:] == "SI").all()
+
+
+def test_get_land_ocean():
+    data = huracanpy.load(huracanpy.example_csv_file)
+    result = huracanpy.utils.geography.get_land_or_ocean(data.lon, data.lat)
+
+    assert (result[:15] == "Ocean").all()
+    assert (result[15:30] == "Land").all()
+    assert (result[30:] == "Ocean").all()
+
+
+def test_get_country():
+    data = huracanpy.load(huracanpy.example_csv_file)
+    result = huracanpy.utils.geography.get_country(data.lon, data.lat)
+
+    assert (result[:15] == "").all()
+    assert (result[15:30] == "Australia").all()
+    assert (result[30:] == "").all()
