2ee082f48b9f20d59e106d3f6366eec7442c0b56
Leo Saffin <string_buster@hotmail.com>
2024-06-03 10:30:41 +0100

    Add test fixture for data with longitude from -180 to 180. Add cartopy transformation in get_basin and redefine basins from -180 to 180 to get a transform that works

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+import datetime
+
+import numpy as np
+import xarray as xr
+
+import huracanpy
+
+
+@pytest.fixture()
+def tracks_csv():
+    return huracanpy.load(huracanpy.example_csv_file)
+
+
+@pytest.fixture()
+def tracks_minus180_plus180():
+    return xr.Dataset(
+        dict(
+            track_id=np.zeros(24),
+            time=[datetime.datetime(2000, 1, 1, n) for n in range(24)],
+            lon=np.linspace(-180, 180, 24),
+            lat=np.linspace(-90, 90, 24),
+        )
+    )
