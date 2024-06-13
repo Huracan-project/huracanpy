5580334a1447da862b7d5114469467fdb87c14c4
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-06-13 16:39:43 +0200

    Implement a wrapper function to add all infos

--- huracanpy/utils/__init__.py
+++ huracanpy/utils/__init__.py
@@ -1,7 +1,79 @@
 """Huracanpy module for useful auxilliary functions"""
 
-__all__ = ["geography", "category", "time"]
+__all__ = ["geography", "category", "time", "add_all_info"]
 
 from . import geography
 from . import category
 from . import time
+
+
+def add_all_info(
+    data,
+    lat_name="lat",
+    lon_name="lon",
+    track_id_name="track_id",
+    time_name="time",
+    year_name="year",
+    month_name="month",
+    day_name="day",
+    hour_name="hour",
+    wind_name="wind10",
+    wind_units="m s-1",
+    slp_name="slp",
+    slp_units="hPa",
+    sshs_convention="Saffir-Simpson",
+    pres_cat_convention="Klotzbach",
+):
+    """
+
+
+    Parameters
+    ----------
+    data : TYPE
+        DESCRIPTION.
+    lat_name : TYPE, optional
+        DESCRIPTION. The default is "lat".
+    lon_name : TYPE, optional
+        DESCRIPTION. The default is "lon".
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Geographical
+    if lat_name is not None:
+        data["hemisphere"] = geography.get_hemisphere(data[lat_name])
+        if lon_name is not None:
+            data["basin"] = geography.get_basin(data[lon_name], data[lat_name])
+            data["is_ocean"] = geography.get_land_or_ocean(
+                data[lon_name], data[lat_name]
+            )
+            data["country"] = geography.get_country(data[lon_name], data[lat_name])
+            data["continent"] = geography.get_continent(data[lon_name], data[lat_name])
+
+    # Time
+    if time_name is not None:
+        if time_name not in list(data.keys()):
+            data[time_name] = time.get_time(
+                data[year_name],
+                data[month_name],
+                data[day_name],
+                data[hour_name],
+            )
+        data["season"] = time.get_season(
+            data[track_id_name], data[lat_name], data[time_name]
+        )
+
+    # Category
+    if wind_name is not None:
+        data["sshs"] = category.get_sshs_cat(
+            data[wind_name], sshs_convention, wind_units
+        )
+    if slp_name is not None:
+        data["pres_cat"] = category.get_pressure_cat(
+            data[slp_name], pres_cat_convention, slp_units
+        )
+
+    return data
