b4ab7ab839e775e11b678e9c1c1172f3fb8db3ee
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-07-12 13:59:34 +0100

    Implement test for rates

--- huracanpy/diags/__init__.py
+++ huracanpy/diags/__init__.py
@@ -1,8 +1,9 @@
 """Huracanpy module for tracks diagnostics"""
 
-__all__ = ["track_density", "track_stats", "translation_speed", "lifecycle"]
+__all__ = ["track_density", "track_stats", "translation_speed", "lifecycle", "rate"]
 
 from . import track_density
 from . import track_stats
 from . import translation_speed
 from . import lifecycle
+from .rates import rate
