199b71d9b0101d1a8cee4b8066e6fc287aa53c68
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-08-13 15:57:59 +0100

    Improve freq function with doc and case management

--- huracanpy/diags/climato.py
+++ huracanpy/diags/climato.py
@@ -9,5 +9,24 @@ def nunique(self):
 xr.DataArray.nunique = nunique
 
 
-def freq(self, by="season", track_id_name="track_id"):
-    return xr.DataArray(self[track_id_name].nunique() / self[by].nunique())
+def freq(self, by=None, track_id_name="track_id"):
+    """
+    Compute the frequency (number of tracks) in a dataset, potentially normalized by another variable (e.g. season to get yearly count).
+
+    Parameters
+    ----------
+    by : str, optional
+        Variable to normalize frequency. The default is None (which means the number of unique track in the whole dataset is provided).
+    track_id_name : str, optional
+        Name of the unique track identifier variable. The default is "track_id".
+
+    Returns
+    -------
+    xr.DataArray
+        The frequency number.
+
+    """
+    if by is None:
+        return xr.DataArray(self[track_id_name].nunique())
+    else:
+        return xr.DataArray(self[track_id_name].nunique() / self[by].nunique())
