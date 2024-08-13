44a192c05a49e5ac81a9e9043ecbad08aa235be7
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-08-13 13:56:50 +0100

    Fix plot tracks to take as argument lon and lat instead of the object

--- huracanpy/plot/tracks.py
+++ huracanpy/plot/tracks.py
@@ -8,21 +8,18 @@ import cartopy.crs as ccrs
 
 
 def plot_tracks_basic(
-    tracks,
+    lon,
+    lat,
     intensity_var=None,
     subplot_kws=dict(projection=ccrs.PlateCarree(180)),
     fig_kws=dict(figsize=(10, 10)),
     scatter_kws=dict(palette="nipy_spectral", s=2, color="k"),
 ):
-    assert "lon" in list(tracks.keys()), "lon is not present in the data"
-    assert "lat" in list(tracks.keys()), "lat is not present in the data"
-
     fig, ax = plt.subplots(subplot_kw=subplot_kws, **fig_kws)
     ax.coastlines()
     sns.scatterplot(
-        data=tracks,
-        x="lon",
-        y="lat",
+        x=lon,
+        y=lat,
         hue=intensity_var,
         ax=ax,
         **scatter_kws,
