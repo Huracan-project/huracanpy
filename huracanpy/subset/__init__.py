22ed17282c897e089ae0e14bc919f35e5b6d19ee
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-08-13 13:53:27 +0100

    More efficient method to sel one track based on id/name

--- huracanpy/subset/__init__.py
+++ huracanpy/subset/__init__.py
@@ -1,3 +1,9 @@
-__all__ = ["trackswhere"]
+__all__ = ["trackswhere", "sel_id"]
 
 from ._where import trackswhere
+
+
+def sel_id(data, tid, track_id_name="track_id"):
+    df = data.to_dataframe()
+    track = df[df[track_id_name] == tid]
+    return track.to_xarray()
