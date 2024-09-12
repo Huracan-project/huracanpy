042c82edecd4cc56a219e1c9693caa6a0da0a638
stella-bourdin <stella.bourdin@physics.ox.ac.uk>
2024-09-12 11:01:56 +0200

    Add geography methods to accessor

--- huracanpy/utils/_xarray.py
+++ huracanpy/utils/_xarray.py
@@ -1,7 +1,13 @@
 import xarray as xr
 
 from huracanpy.subset import trackswhere
-from .geography import get_hemisphere
+from .geography import (
+    get_hemisphere,
+    get_basin,
+    get_land_or_ocean,
+    get_country,
+    get_continent,
+)
 
 
 @xr.register_dataset_accessor("huracanpy")
@@ -14,3 +20,37 @@ class HuracanpyAccessor:
 
     def get_hemisphere(self, lat_name="lat"):
         return get_hemisphere(self._dataset[lat_name])
+
+    def get_basin(self, lon_name="lon", lat_name="lat", convention="WMO", crs=None):
+        return get_basin(
+            self._dataset[lon_name],
+            self._dataset[lat_name],
+            convention=convention,
+            crs=crs,
+        )
+
+    def get_land_or_ocean(
+        self, lon_name="lon", lat_name="lat", resolution="10m", crs=None
+    ):
+        return get_land_or_ocean(
+            self._dataset[lon_name],
+            self._dataset[lat_name],
+            resolution=resolution,
+            crs=crs,
+        )
+
+    def get_country(self, lon_name="lon", lat_name="lat", resolution="10m", crs=None):
+        return get_country(
+            self._dataset[lon_name],
+            self._dataset[lat_name],
+            resolution=resolution,
+            crs=crs,
+        )
+
+    def get_continent(self, lon_name="lon", lat_name="lat", resolution="10m", crs=None):
+        return get_continent(
+            self._dataset[lon_name],
+            self._dataset[lat_name],
+            resolution=resolution,
+            crs=crs,
+        )
