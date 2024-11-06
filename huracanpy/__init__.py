cda45c950a6b7377743a4c363b057607a808d55d
Leo Saffin <string_buster@hotmail.com>
2024-11-06 14:54:54 +0000

    Remove duplicated information in conf.py and top level __init__.py. Ideally all information should come from pyproject.toml

--- huracanpy/__init__.py
+++ huracanpy/__init__.py
@@ -1,7 +1,5 @@
 """huracanpy - A python package for working with various forms of feature tracking data"""
 
-__version__ = "0.1.0"
-__author__ = "Leo Saffin <l.saffin@reading.ac.uk>, Stella Bourdin <stella.bourdin@physics.ox.ac.uk>, Kelvin Ng "
 __all__ = [
     # Modules
     "calc",
