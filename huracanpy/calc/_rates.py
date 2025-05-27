Increase test coverage where possible and fix a few errors. 1. Consistent use of dummy track_id in rates/translation when not specified 2. Correctly convert units for category, when they are only included for one variable 3. Always convert units to metres in beta_drift

huracanpy/calc/_rates.py
@@ -5,7 +5,6 @@ Module containing functions to compute rates.
 import warnings
 
 import numpy as np
-import xarray as xr
 from metpy.units import units
 from metpy.xarray import preprocess_and_wrap
 
@@ -87,7 +86,7 @@ def rate(var, time, track_ids=None, centering="forward"):
     # Curate input
     # If track_id is not provided, all points are considered to belong to the same track
     if track_ids is None:
-        track_ids = xr.DataArray([0] * len(time), dims=time.dims)
+        track_ids = np.zeros(var.shape)
         warnings.warn(
             "track_id is not provided, all points are considered to come from the same"
             "track"

huracanpy/calc/_translation.py
@@ -138,9 +138,15 @@ def distance(lon, lat, *args, track_id=None, method="geod", ellps="WGS84"):
         lat2 = lat[1:]
 
         if len(args) == 1:
-            track_id = args[0]
+            if track_id is None:
+                track_id = args[0]
+            else:
+                raise ValueError(
+                    "Distance either takes 2 arrays (lon/lat) or 4 arrays 2x(lon/lat)"
+                )
 
         if track_id is None:
+            track_id = np.zeros(lon.shape)
             warnings.warn(
                 "track_id is not provided, all points are considered to come from the"
                 "same track"
@@ -203,14 +209,14 @@ def translation_speed(lon, lat, time, track_id=None, method="geod", ellps="WGS84
     # Curate input
     # If track_id is not provided, all points are considered to belong to the same track
     if track_id is None:
-        np.zeros_like(lon)
+        track_id = np.zeros(lon.shape)
         warnings.warn(
             "track_id is not provided, all points are considered to come from the same"
             "track"
         )
 
     # Distance between each points
-    dx = distance(lon, lat, track_id, method=method, ellps=ellps)
+    dx = distance(lon, lat, track_id=track_id, method=method, ellps=ellps)
 
     # time between each points
     dt = delta(time, track_id)

huracanpy/tc/_beta_drift.py
@@ -52,14 +52,14 @@ def beta_drift(
     if not isinstance(wind_max, pint.Quantity) or wind_max.unitless:
         wind_max = wind_max * units("m / s")
 
-    ## Convert rmw to m
+    # Convert rmw to m
     if not isinstance(radius_wind_max, pint.Quantity) or radius_wind_max.unitless:
-        if (
-            radius_wind_max.max() < 10000
-        ):  # We assume rmw are in km if they are below 10,000
-            radius_wind_max = radius_wind_max * 1000
-
-        radius_wind_max = radius_wind_max * units("m")
+        if radius_wind_max.max() < 10000:
+            # We assume rmw are in km if they are below 10,000
+            radius_wind_max = radius_wind_max * units("km")
+        else:
+            radius_wind_max = radius_wind_max * units("m")
+    radius_wind_max = radius_wind_max.to("m")
 
     # Coriolis parameter
     beta = 2 * omega * np.cos(lat) / Re  # s-1 m-1

tests/test_tc/test_ace.py
@@ -1,35 +1,61 @@
-import pytest
+from metpy.units import units
 import numpy as np
+import pytest
 
 import huracanpy
 
 
 @pytest.mark.parametrize(
-    "sum_by, result",
-    [(None, [10.0894797]), ("track_id", [3.03623809, 2.21637375, 4.83686787])],
+    "threshold, sum_by, result",
+    [
+        (34 * units("knots"), None, [10.0894797]),
+        ((34 * units("knots")).to("m s-1"), None, [10.0894797]),
+        ((34 * units("knots")).to("m s-1").magnitude, None, [10.0894797]),
+        (0, None, [13.23391871]),
+        (34 * units("knots"), "track_id", [3.03623809, 2.21637375, 4.83686787]),
+    ],
 )
-def test_ace(sum_by, result):
-    data = huracanpy.load(huracanpy.example_csv_file, source="csv")
-
+def test_ace(tracks_csv, threshold, sum_by, result):
     if sum_by is not None:
-        sum_by = data[sum_by]
-    ace = huracanpy.tc.ace(data.wind10, sum_by=sum_by)
+        sum_by = tracks_csv[sum_by]
+
+    ace = huracanpy.tc.ace(tracks_csv.wind10, sum_by=sum_by, threshold=threshold)
+
     if sum_by is None:
         ace = ace.sum()
 
     np.testing.assert_allclose(ace, result)
-
     assert isinstance(ace.data, np.ndarray)
 
 
-def test_pace():
-    data = huracanpy.load(huracanpy.example_csv_file, source="csv")
-    # Pass wind values to fit a (quadratic) model to the pressure-wind relationship
-    pace, model = huracanpy.tc.pace(data.slp, data.wind10, sum_by=data.track_id)
+@pytest.mark.parametrize(
+    "threshold_pressure, threshold_wind, sum_by, result",
+    [
+        (None, None, "track_id", [4.34978137, 2.65410482, 6.09892875]),
+    ],
+)
+def test_pace(tracks_csv, threshold_pressure, threshold_wind, sum_by, result):
+    if sum_by is not None:
+        sum_by = tracks_csv[sum_by]
 
-    np.testing.assert_allclose(pace, np.array([4.34978137, 2.65410482, 6.09892875]))
+    # Pass wind values to fit a (quadratic) model to the pressure-wind relationship
+    pace, model = huracanpy.tc.pace(
+        tracks_csv.slp,
+        tracks_csv.wind10,
+        sum_by=sum_by,
+        threshold_wind=threshold_wind,
+        threshold_pressure=threshold_pressure,
+    )
+
+    np.testing.assert_allclose(pace, result)
 
     # Call with the already fit model instead of wind values
-    pace, _ = huracanpy.tc.pace(data.slp, model=model, sum_by=data.track_id)
+    pace, _ = huracanpy.tc.pace(
+        tracks_csv.slp,
+        model=model,
+        sum_by=sum_by,
+        threshold_wind=threshold_wind,
+        threshold_pressure=threshold_pressure,
+    )
 
     np.testing.assert_allclose(pace, np.array([4.34978137, 2.65410482, 6.09892875]))