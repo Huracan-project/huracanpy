6583f4676d44b09cab275935a0a70e47d97b494b
Leo Saffin <string_buster@hotmail.com>
2025-07-10 10:36:42 +0100

    Use CSV reader for TempestExtremes

--- huracanpy/_data/_tempestextremes.py
+++ huracanpy/_data/_tempestextremes.py
@@ -1,7 +1,6 @@
-import datetime
+from io import StringIO
 
-import numpy as np
-import xarray as xr
+from . import _csv
 
 
 def load(
@@ -10,14 +9,10 @@ def load(
     tempest_extremes_unstructured=False,
     tempest_extremes_header_str="start",
 ):
-    output = list()
-
     with open(filename, "r") as f:
         data = f.readlines()
 
     lineno = 0
-    # TempestExtremes ASCII does not have a track_id, so just use a counter variable
-    track_id = 0
 
     # Just in case there are any empty lines at the start of the file
     # This can probably be deleted
@@ -34,8 +29,7 @@ def load(
         varnames.insert(1, "j")
 
     if variable_names is None:
-        for i in range(nfields - len(varnames) - 4):
-            varnames.append(f"feature_{i}")
+        varnames += [f"feature_{i}" for i in range(nfields - len(varnames) - 4)]
     else:
         nvars = nfields - len(varnames) - 4
         if len(variable_names) != nvars:
@@ -45,39 +39,21 @@ def load(
             )
         varnames += variable_names
 
+    # TempestExtremes ASCII does not have a track_id, so just use a counter variable
+    track_id = 0
+    # Last four columns are always year, month, day, hour
+    varnames = ["track_id"] + varnames + ["year", "month", "day", "hour"]
+
+    output = [",".join(varnames)]
     while lineno < len(data):
         start, npoints, year, month, day, hour = data[lineno].split()
         npoints = int(npoints)
 
-        # Create the empty data for the new track
-        times = [None] * npoints
-        track_data = {label: ("record", np.zeros(npoints)) for label in varnames}
-
-        track_id_array = np.asarray([track_id] * npoints)
-        track_data["track_id"] = ("record", track_id_array)
-
         # Populate time and data line by line
         for m in range(npoints):
-            line = data[lineno + 1 + m].split()
-
-            for i, name in enumerate(varnames):
-                track_data[name][1][m] = float(line[i])
-
-            year, month, day, hour = line[-4:]
-            time = datetime.datetime(int(year), int(month), int(day), int(hour))
-            times[m] = time
-
-        # Return a dataset for the individual track
-        # Add time separately so xarray can deal with the awkward np.datetime64
-        # format
-        ds = xr.Dataset(track_data)
-        ds["time"] = ("record", times)
-        output.append(ds)
+            output.append(",".join([str(track_id)] + data[lineno + 1 + m].split()))
 
         track_id += 1
         lineno += npoints + 1
 
-    output = xr.concat(output, dim="record")
-    output.track_id.attrs["cf_role"] = "trajectory_id"
-
-    return output
+    return _csv.load(StringIO("\n".join(output)), index_col=False)
