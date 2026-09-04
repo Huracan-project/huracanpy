0491d9ef0797f60c23ca7cbc2b5120f935c9a3bb
Leo Saffin <string_buster@hotmail.com>
2026-09-04 16:05:17 +0100

    Update documentation examples for density

--- huracanpy/calc/_density.py
+++ huracanpy/calc/_density.py
@@ -72,7 +72,7 @@ def density(
     function_kws : dict
         Keyword arguments passed to the function used for calculating density
 
-        * If method="histogram", :func:`numpy.histogram2d`
+        * If method="histogram" or "line", :func:`numpy.histogram2d`
         * If method="kde" and spherical=`True`,
           :class:`sklearn.neighbors.KernelDensity`. Note that the bandwidth argument is
           set to `"scott"` rather than the default of `1.0`
